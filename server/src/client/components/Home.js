8e7aa750655d7615d0d1698b57f5ec1f6293b811
gitAmelio <ameliosconnection@gmail.com>
2021-06-29 06:42:53 +0200

    refactored for cleaner code

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>I'm the Very best home component</div>
+            <button onClick={() => console.log('Hi There')}>Press me!</button>
+        </div>
+
+    )
+}
+
+export default Home;
